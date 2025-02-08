55303c34d3a14e0bb678a315a50eccb58cbd1605
Sudheerkumar22 <sudheerkumarn22@gmail.com>
2025-02-08 18:08:24 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,68 @@
+function showAbout() {
+    document.getElementById("about").style.display = "block";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("education").style.display = "none";
+    document.getElementById("internship").style.display = "none";
+    document.getElementById("contactdetails").style.display = "none";
+    document.getElementById("resume").style.display = "none";
+}
+
+function showSkills() {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("skills").style.display = "block";
+    document.getElementById("education").style.display = "none";
+    document.getElementById("internship").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("contactdetails").style.display = "none";
+    document.getElementById("resume").style.display = "none";
+}
+
+function showEducation() {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("education").style.display = "block";
+    document.getElementById("internship").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("contactdetails").style.display = "none";
+    document.getElementById("resume").style.display = "none";
+}
+
+function showInternship() {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("education").style.display = "none";
+    document.getElementById("internship").style.display = "block";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("contactdetails").style.display = "none";
+    document.getElementById("resume").style.display = "none";
+}
+
+function showProjects() {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("education").style.display = "none";
+    document.getElementById("internship").style.display = "none";
+    document.getElementById("projects").style.display = "block";
+    document.getElementById("contactdetails").style.display = "none";
+    document.getElementById("resume").style.display = "none";
+}
+
+function showContactdetails() {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("education").style.display = "none";
+    document.getElementById("internship").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("contactdetails").style.display = "block";
+    document.getElementById("resume").style.display = "none";
+}
+
+function showResume() {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("education").style.display = "none";
+    document.getElementById("internship").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+    document.getElementById("contactdetails").style.display = "none";
+    document.getElementById("resume").style.display = "block";
+}
